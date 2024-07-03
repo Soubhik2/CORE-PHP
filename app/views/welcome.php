9fd352be0a4ee2351bfdd51150b97ef11b0e22e9
Soubhik <soubhik960@gmail.com>
2024-07-03 22:00:50 +0530

    fix errors

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link rel="icon" type="image/x-icon" href="<?= BASEURL ?>public/asset/php.png">
-    <link rel="stylesheet" href="<?= BASEURL ?>public/css/demo.css">
+    <!-- <link rel="stylesheet" href="<?= BASEURL ?>public/css/demo.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   </head>
@@ -33,10 +33,6 @@
             <li class="list-group-item">$pass_url : <?= $pass_url ?></li>
         </ul>
     </div>
-
-    <?php
-      require 'name'
-    ?>
 
     <?php require 'util/footer.php'; ?>
 
